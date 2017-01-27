5113a44fa1f6ff750457493c9079f6fa61a1d347
christaborium <dxdstudio@gmail.com>
2017-01-27 12:04:28 -0800

    Cleanup ui and example app

--- app.py
+++ app.py
@@ -2,15 +2,11 @@
 
 import os
 import json
-from pprint import pprint as ppr
 
 from flask import (
     Flask,
-    Blueprint,
     render_template,
-    url_for,
     request,
-    flash,
 )
 
 from yaml import load
@@ -27,28 +23,42 @@ FlaskExtras(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """Homepage."""
-    spec = load(open(cwd + '/example_specs/uber.yaml', 'r').read())
+    filename = 'petstore-expanded.yaml'
+    filepath = cwd + '/example_specs/' + filename
+    spec = load(open(filepath, 'r').read())
     paths = spec.get('paths')
-    product_form = swagger_gen.get_form_from_path('/products', 'GET', paths)
+    product_form = swagger_gen.get_form_from_path('/pets', 'get', paths)
     form = product_form()
     if request.method == 'POST':
         # Trigger errors for testing
         form.validate_on_submit()
-    kwargs = dict(url='/products', form=form, name='Product form')
+    kwargs = dict(
+        url='/pets',
+        spec=json.dumps(spec, indent=4),
+        form=form,
+        file=filename,
+        name='Endpoint form'
+    )
     return render_template('pages/index.html', **kwargs)
 
 
 @app.route('/multiple', methods=['GET', 'POST'])
 def multiple():
     """Multiple forms for all defs."""
-    spec = open(cwd + '/example_specs/uber.yaml', 'r').read()
-    defs = load(spec).get('definitions')
+    filename = 'uber.yaml'
+    filepath = cwd + '/example_specs/' + filename
+    spec = load(open(filepath, 'r').read())
+    defs = spec.get('definitions')
     forms = swagger_gen.get_forms_from_defs(defs, exclude=['Error'])
     if request.method == 'POST':
         # Trigger errors for testing
         for _, form in forms.items():
             form.validate_on_submit()
-    kwargs = dict(forms=forms, spec=json.dumps(defs, indent=4))
+    kwargs = dict(
+        forms=forms,
+        file=filename,
+        spec=json.dumps(spec, indent=4),
+    )
     return render_template('pages/multiple.html', **kwargs)
 
 
